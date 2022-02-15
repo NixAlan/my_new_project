fd5a4780fb9d91324ce99238bb3f70dc529222f9
nixalan <nixalan@gmail.com>
2022-02-15 17:09:29 -0600

    todo connect server with client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import PersonForm from "./Components/PersonForm";
+
+function App() {
+  return (
+    <div className="App">
+      <PersonForm />
+    </div>
+  );
+}
+
+export default App;
